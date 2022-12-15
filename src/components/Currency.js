ad2ad9dff7e653e247709455d04496f24f618db6
davegosbee <davidlgosbee@gmail.com>
2022-12-14 22:53:02 -0500

    styling

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -24,15 +24,15 @@ const Currency = () => {
 	return (
 		<>
         
-        <div className="dropdown">
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        <div className="dropdown alert alert-primary">
+        <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
         Currency ({currency} {currencyDesc})
         
         </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" style={{ backgroundColor: "#198754"}}>
             {
             currencies.map((c) => (
-                <li key={c.symbol}><a className="dropdown-item" href="#" onClick={() => handleSetCurrency2(c.symbol)}>{c.symbol} {c.description}</a></li>
+                <li key={c.symbol}><a className="dropdown-item" href="#" onClick={() => handleSetCurrency2(c.symbol)} >{c.symbol} {c.description}</a></li>
 
             ))}
           
